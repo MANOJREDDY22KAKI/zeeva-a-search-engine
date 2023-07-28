b0a6ed97af06a82f96e1e3ecf0771753d32c8d6b
MANOJREDDY22KAKI <114500520+MANOJREDDY22KAKI@users.noreply.github.com>
2023-07-28 13:02:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ZEEVA Web Crawler</title>
+
+	<meta name="description" content="Search the web for sites and images.">
+	<meta name="keywords" content="search engine, zeeva, websites">
+	<meta name="author" content="ZEEVA">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="icon" type="image/x-icon" href="assets/images/favicon/Z.ico">
+	<link rel="shortcut icon" type="image/jpg" href="assets/images/favicon/Z 32 32.jpg">
+	<link rel="apple-touch-icon" href="assets/images/favicon/apple-touch-icon.png">
+	<link rel="android-chrome-icon" type="image/jpg" href="assets/images/favicon/Z 512 512.jpg">
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+			<div class="logoContainer">
+				<img src="assets\images\zeevaLogo.jpg" height="150px" width="150px" title="Logo of our site" alt="Site logo"  >
+				
+			</div>
+
+			<div class="searchContainer">
+				<form action="search.php" method="GET">
+					<input class="searchBox" type="text" name="term" autocomplete="off">
+					<input class="searchButton" type="submit" value="Search">
+				</form>
+			</div>
+		</div>
+    </div>
+</body>
+</html>
